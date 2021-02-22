050eb4485ee4094767bdc8d89cb98e488682be0b
李帅 <136859891@qq.com>
2021-02-22 15:11:46 +0800

    更新

--- GenericProjectForSwift/Pods/ZLPhotoBrowser/Sources/Extensions/Int+ZLPhotoBrowser.swift
+++ GenericProjectForSwift/Pods/ZLPhotoBrowser/Sources/Extensions/Int+ZLPhotoBrowser.swift
@@ -1,10 +1,10 @@
 //
-//  Kingfisher.h
-//  Kingfisher
+//  Int+ZLPhotoBrowser.swift
+//  ZLPhotoBrowser
 //
-//  Created by Wei Wang on 15/4/6.
+//  Created by ruby109 on 2020/11/3.
 //
-//  Copyright (c) 2019 Wei Wang <onevcat@gmail.com>
+//  Copyright (c) 2020 Long Zhang <495181165@qq.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -24,14 +24,12 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+import Foundation
 
-//! Project version number for Kingfisher.
-FOUNDATION_EXPORT double KingfisherVersionNumber;
-
-//! Project version string for Kingfisher.
-FOUNDATION_EXPORT const unsigned char KingfisherVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Kingfisher/PublicHeader.h>
-
-
+extension Int {
+    
+    init(_ bool:Bool) {
+        self = bool ? 1 : 0
+    }
+    
+}
